db6fa07c69043a6ac84c31ec853e6fbf8ab3b7e4
ababaSigrun <ababa715git@gmail.com>
2020-11-30 09:08:45 +0000

    htmlにする

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ var fs   = require('fs');
 var http_server = new http.createServer(function(req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end(fs.readFileSync('./index.html', 'utf-8'));
-}).listen(3000);
+}).listen(5000));
 console.log('Server running at http://localhost:5000/');
