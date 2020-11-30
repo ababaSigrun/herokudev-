b35b6c60d5fc9798e8718eab14ee8afe88271d7d
ababaSigrun <ababa715git@gmail.com>
2020-11-30 09:33:06 +0000

    一回全部削除

--- .heroku.dockerfile
+++
@@ -1,5 +0,0 @@
-FROM node:12.13
-WORKDIR /app
-COPY . /app
-RUN npm i
-CMD npm start
--- index.js
+++
@@ -1,8 +0,0 @@
-let port = process.env.PORT || 5000;
-const http = require('http');
-var fs   = require('fs');
-var http_server = new http.createServer(function(req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end(fs.readFileSync('./index.html', 'utf-8'));
-}).listen(5000);
-console.log('Server running at http://localhost:5000/');
