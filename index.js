d3ff58bf518198b858643fad8b051098f7b98f2c
ababaSigrun <ababa715git@gmail.com>
2020-11-30 08:57:39 +0000

    htmlにする

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let port = process.env.PORT || 5000;
 const http = require('http');
-http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World   a \n');
-}).listen(port, () => console.log('Server http://localhost:'+port));
+var fs   = require('fs');
+var http_server = new http.createServer(function(req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end(fs.readFileSync('./index.html', 'utf-8'));
+}).listen(3000);
+console.log('Server running at http://localhost:3000/');
