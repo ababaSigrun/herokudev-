95bc2d74e1411fa1d638b056f278d8408b7e34a1
ababaSigrun <ababa715git@gmail.com>
2020-12-11 09:15:47 +0000

    chart.jsのテスト

---
+++ script/chartData.js
@@ -0,0 +1,12 @@
+  const data = {
+    labels: ['A', 'B', 'C', 'D', 'E'],
+    datasets: [{
+      label: '国語のテスト',
+      data: [78, 64, 35, 90, 83]
+    }]}
+  // グラフを描画 --- (*2)
+  const ctx = document.getElementById('chart_cv')
+  const chart_cv = new Chart(ctx, {
+    type: 'bar', // グラフの種類
+    data: data, // データ
+    options: {}}) // オプション
