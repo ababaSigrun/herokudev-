898f7ada23628ff74a1f3df1ac0bd17d378f2fac
ababaSigrun <ababa715git@gmail.com>
2020-12-21 09:49:26 +0000

    グラフでDBも表示するようにした。

--- script/chartData.js
+++ script/chartData.js
@@ -1,17 +1,32 @@
 const progarmData = {
-    labels: ['Java', 'VBA', 'C', 'COBOL', 'Javascript', 'Python', 'Ruby'],
+    labels: ['Java', 'VBA', 'C', 'COBOL', 'Javascript', 'Python', '4D'],
     datasets: [{
         label: '習得レベル',
         data: [5, 4.8, 3.5, 3.5, 2.5, 2, 1.5],
-        backgroundColor: ['red', 'green', 'blue', 'pink', 'black', 'yellow', 'lightyellow']
+        backgroundColor: ['lightseagreen', 'darkturquoise', 'mediumturquoise', 'turquoise', 'palegreen', 'springgreen', 'mediumseagreen']
     }]
 }
+const detaBaseData = {
+    labels: ['MariaDB', 'OracleDB', 'Access', '4D', 'SQLServer'],
+    datasets: [{
+        label: '習得レベル',
+        data: [5, 4.5, 2.0, 1.5, 0.5],
+        backgroundColor: ['lightseagreen', 'darkturquoise', 'mediumturquoise', 'turquoise', 'palegreen'],
+        borderWidth: 1
+    }]
+}
+
+
+
 // グラフを描画 --- (*2)
 
 function setSkillMode(mode) {
     // alert(mode);
     switch (mode) {
         case "progarm":
+            if (window.chart_cv instanceof Chart) {
+                chart_cv.destroy();
+            }
             ctx = document.getElementById('chart_cv')
             chart_cv = new Chart(ctx, {
                 type: 'bar', // グラフの種類
@@ -19,17 +34,11 @@ function setSkillMode(mode) {
                 options: {
                     scales: {                          //軸設定
                         yAxes: [{                      //y軸設定
-                            display: true,             //表示設定
-                            scaleLabel: {              //軸ラベル設定
-                                display: true,          //表示設定
-                                labelString: '習得レベル',  //ラベル
-                                fontSize: 18               //フォントサイズ
-                            },
                             ticks: {                      //最大値最小値設定
                                 min: 0,                   //最小値
                                 max: 5,                  //最大値
                                 fontSize: 18,             //フォントサイズ
-                                stepSize: 5               //軸間隔
+                                stepSize: 1               //軸間隔
                             },
                         }],
                     }
@@ -38,11 +47,39 @@ function setSkillMode(mode) {
             break;
 
         case "detaBase":
-            chart_cv.destroy();
+            if (window.chart_cv instanceof Chart) {
+                chart_cv.destroy();
+            }
+
+            ctx = document.getElementById('chart_cv')
+            chart_cv = new Chart(ctx, {
+                type: 'bar', // グラフの種類
+                data: detaBaseData, // データ
+                options: {
+                    scales: {                          //軸設定
+                        yAxes: [{                      //y軸設定
+                            ticks: {                      //最大値最小値設定
+                                min: 0,                   //最小値
+                                max: 5,                  //最大値
+                                fontSize: 18,             //フォントサイズ
+                                stepSize: 1               //軸間隔
+                            },
+                        }],
+                    }
+                }
+            })
             break;
 
         case "tool":
-            chart_cv.destroy();
+            if (window.chart_cv instanceof Chart) {
+                chart_cv.destroy();
+            }
+
+
+            // エラーを握りつぶす。ここ何かいい書き方できるはず。
+
+
+
             break;
     }
 }
