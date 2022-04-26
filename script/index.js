0aa6a939ffa20685ce3bb7cd891df054102e9af5
ababaSigrun <ababa715git@gmail.com>
2022-04-26 04:13:27 +0000

    アコーディオン骨組み作成

--- script/index.js
+++ script/index.js
@@ -77,7 +77,58 @@ function ageCalculation() {
  * @param {*} no 
  */
 function information(no) {
- //  console.log(no);
- alert(no + " :まだ未実装 アコーディオンで実装する。");
+    // ここの部分はcace文で切り分けること。
+    let elem;
+    switch (no) {
+        case 1:
+            elem = document.getElementById('detail1');
+            break;
+        case 2:
+            elem = document.getElementById('detail2');
+            break;
+        case 3:
+            elem = document.getElementById('detail3');
+            break;
+        case 4:
+            elem = document.getElementById('detail4');
+            break;
+        case 5:
+            elem = document.getElementById('detail5');
+            break;
+        case 6:
+            elem = document.getElementById('detail6');
+            break;
+        case 7:
+            elem = document.getElementById('detail7');
+            break;
+        case 8:
+            elem = document.getElementById('detail8');
+            break;
+        case 9:
+            elem = document.getElementById('detail9');
+            break;
+        case 10:
+            elem = document.getElementById('detail10');
+            break;
+        case 11:
+            elem = document.getElementById('detail11');
+            break;
+        case 12:
+            elem = document.getElementById('detail12');
+            break;
+        case 13:
+            elem = document.getElementById('detail13');
+            break;
+        default:
+            console.log('作ってないよ');
+    }
 
+
+
+
+    if (elem.hidden == false) {
+        elem.hidden = true;
+    } else {
+        elem.hidden = false;
+    }
 }
